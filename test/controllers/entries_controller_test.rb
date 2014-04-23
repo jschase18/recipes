4991c7bf1373a277a82deba5cba49c30a17e23fb
David Onken <David.Onken@utah.edu>
2014-04-23 16:36:42 -0600

    Testing functionality

--- test/controllers/entries_controller_test.rb
+++ test/controllers/entries_controller_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class EntriesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	#test "the truth" do
+	#	assert true
+	#end
+	
+	setup :initialize_entry
+	
+	def teardown
+		@entry = nil
+	end
+	
+	test "should create entry" do
+		assert_difference('Entry.count') do
+			post :create, entries: {name: 'Testname', description: '1234567890', ingredients: '1234567890', instructions: '1234567890'}
+		end
+	
+	assert_redirected_to entry_path(assigns(:entry))
+	end
+	
+	#test "should update post"
+	
+	#test "should destroy entry" do
+	#	assert_difference('Entry.count', -1) do
+	#		delete :destroy, id: @entry.id
+	#	end
+	#
+	#assert_redirected_to entries_path
+	#end
+	
+	test "should show entry" do
+		get :show, id: @entry.id
+		assert_response :success
+	end
+	
+	test "should get index" do
+		get :index
+		assert_response :success
+		assert_not_nil assigns(:entries)
+	end
+	
+	private
+		def initialize_entry
+			@entry = entries(:one)
+		end
 end
--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -4,4 +4,55 @@ class EntryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+	test "should not save without name" do
+		entry = Entry.new
+		assert_not entry.save, "Saved the entry without a name"
+	end
+	
+	test "Should not save without a description"do
+		entry = Entry.new
+		entry.name = "TestName"
+		entry.instructions = "Testing123456789"
+		entry.ingredients = "Testing123456789"
+		assert_not entry.save, "Saved the entry without a description"
+	end
+	
+	test "Should not save with a <10 character description"do
+		entry = Entry.new
+		entry.name = "TestName"
+		entry.description = "Nope"
+		entry.instructions = "Testing123456789"
+		entry.ingredients = "Testing123456789"
+		assert_not entry.save, "Saved the entry with a short description"
+	end
+	
+	test "Should not save with a <10 character ingredients"do
+		entry = Entry.new
+		entry.name = "TestName"
+		entry.description = "Testing123456789"
+		entry.instructions = "Testing123456789"
+		entry.ingredients = "Nope"
+		assert_not entry.save, "Saved the entry with a short ingredients"
+	end
+	
+	test "Should not save with a <10 character instructions"do
+		entry = Entry.new
+		entry.name = "TestName"
+		entry.description = "Testing123456789"
+		entry.instructions = "Nope"
+		entry.ingredients = "Testing123456789"
+		assert_not entry.save, "Saved the entry with a short instructions"
+	end
+	
+	test "should save properly" do
+		entry = entries(:one)
+		assert entry.save, "Did not save the recipe"
+	end
+	
+	test "should not save properly" do
+		entry = entries(:two)
+		assert_not entry.save, "Saved the invalid recipe"
+	end
+  
 end
