de6f474f79d10f83ff80a3157852ca3fa32a22c5
David Onken <David.Onken@utah.edu>
2014-04-23 12:36:58 -0600

    Comments and a clickable header

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,5 @@
 class Entry < ActiveRecord::Base
+	# This gives us server-side validation
 	validates :name, presence: true, length: { minimum: 3 }
 	validates :description, presence: true, length: { minimum: 10 }
 	validates :ingredients, presence: true, length: { minimum: 10 }
